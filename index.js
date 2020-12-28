bc5d5b2a7d64891bd2d69ce2f6d518334279ecc8
Xuan Hinh <xuanhinh@gmail.com>
2020-12-29 05:12:20 +0700

    git add all file to github

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var express = require('express')
 var app = express()
+var ejs = require('ejs')
 
-app.get('/',(req,res)=>res.send('<h1 style="color: red" align="center">Home page</h1><br><h2 align="center"><a href="/about">about</a></h2>'))
-app.get('/about',(req,res)=>res.send('<h1 style="color: green" align="center">About page</h1><br><h2 align="center"><a href="/">home</a></h2>'))
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+
+app.get('/',(req,res)=>res.render('home.ejs'))
+app.get('/about',(req,res)=>res.render('aboutpage.ejs'))
 
 app.listen(process.env.PORT||8080)
